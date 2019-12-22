30381519b5a7c5cefbcd45531a68335cb2655b0c
Juliano Sirtori <julianosirtori@gmail.com>
2019-12-22 14:29:18 -0300

    First Commit

---
+++ src/components/TechList/index.js
@@ -0,0 +1,26 @@
+import React, {useState} from 'react';
+
+// import { Container } from './styles';
+
+export default function TechList() {
+  const [techs, setTechs] = useState([]);
+  const [newTech, setNewTech] = useState('');
+  
+  function handleAddTech(){
+    setTechs([...techs, newTech]);
+    setNewTech('');
+  }
+
+  return (
+    <form data-testid="tech-form" onSubmit={handleAddTech}>
+      <ul data-testid="tech-list">
+        {techs.map(item => <li key={item}>{item}</li>)}
+      </ul>
+      <label htmlFor="tech">Tech</label>
+      <input id="tech" value={newTech} onChange={e => setNewTech(e.target.value)} />
+      
+      <button onClick={handleAddTech}>Adicionar</button>
+      
+    </form>
+  );
+}
