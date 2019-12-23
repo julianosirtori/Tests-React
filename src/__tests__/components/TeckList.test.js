405b49f59501bbb719fb6e3f33a7a99ca821f1b2
Juliano Sirtori <julianosirtori@gmail.com>
2019-12-23 05:42:06 -0300

    Mock use selector

--- src/__tests__/components/TeckList.test.js
+++ src/__tests__/components/TeckList.test.js
@@ -1,9 +1,13 @@
 import React from 'react';
-import {render, fireEvent} from '@testing-library/react';
+import {render, fireEvent, cleanup} from '@testing-library/react';
 
 import TechList from '~/components/TechList';
 
 describe('TechList component', () => {
+  beforeEach(() => {
+    localStorage.clear();
+  })
+  
   it('should be able to add new tech', () => {
     const { getByText, getByTestId, getByLabelText } =  render(<TechList />);
 
@@ -12,6 +16,21 @@ describe('TechList component', () => {
 
     expect(getByTestId('tech-list')).toContainElement(getByText('Node.js'));
     expect(getByLabelText('Tech')).toHaveValue('');
+
+  });
+
+  it('shoul store techs in storage', () => {
+    let { getByTestId, getByLabelText, getByText } =  render(<TechList />);
+
+    fireEvent.change(getByLabelText('Tech'), {target: {value: 'Node.js'}});
+    fireEvent.submit(getByTestId('tech-form'));
+
+    cleanup();
+    
+    ({getByTestId, getByLabelText, getByText } =  render(<TechList />));
+
+    expect(localStorage.setItem).toHaveBeenCalledWith('techs', JSON.stringify(['Node.js']));
+    expect(getByTestId('tech-list')).toContainElement(getByText('Node.js'));
 
   })
 })
